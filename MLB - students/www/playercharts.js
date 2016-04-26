1d4e1a332bc477b8e06c81efaa6ce58a51c59c2f
Joel D. Sabol <jsabol@cedarville.edu>
2016-04-26 09:27:54 -0400

    Updates to player charts

--- MLB - students/www/playercharts.js
+++ MLB - students/www/playercharts.js
@@ -50,6 +50,21 @@ $(document).ready(function(){
 			}
 		}
 	});
+	Highcharts.chart({
+		chart:{
+			type: 'column',
+			renderTo: 'gamesplayed'
+		},
+		title:{
+			text: 'Games Played'
+		},
+		yAxis:{
+			min:0,
+			title:{
+				text: '#Games Played'
+			}
+		}
+	});
 
 	$.get("http://localhost:5555/player.json", {id:playerID, action:'details'}, function(data){
 		console.log(data);
@@ -58,12 +73,14 @@ $(document).ready(function(){
 		var hits = [];
 		var atbats = [];
 		var homeruns = [];
+		var gamesplayed = [];
 		data.items.forEach(function(item, idx){
-			batavg.push(parseFloat(item.battingavg));
+			batavg.push(parseFloat(item.battingavg)||0);
 			year.push(item.year.toString());
 			hits.push(item.hits);
 			atbats.push(item.atbats);
 			homeruns.push(item.homeruns);
+			gamesplayed.push(item.gamesplayed);
 		});
 		Highcharts.charts.forEach(function(item, index){
 			item.xAxis[0].setCategories(year)
@@ -72,6 +89,7 @@ $(document).ready(function(){
 		Highcharts.charts[1].addSeries({name:'Hits', data:hits}, false);
 		Highcharts.charts[1].addSeries({name:'At Bats', data:atbats});
 		Highcharts.charts[2].addSeries({name:'Home Runs', data:homeruns});
+		Highcharts.charts[3].addSeries({name:'Games Played', data:gamesplayed});
 
 	}, 'json');
 });
